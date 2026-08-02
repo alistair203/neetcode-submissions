31e0e91dc4249a9ce63a909b7cfc1d140bd89166
alistair203 <141773465+alistair203@users.noreply.github.com>
2026-08-03 00:27:39 +0100

    Add: find-the-town-judge - submission-3

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        delta = [0] * n
+        for t in trust:
+            delta[t[0] - 1] -= 1
+            delta[t[1] - 1] += 1
+        for i in range(n):
+            if delta[i] == n - 1:
+                return i + 1
+        return -1
+
+        
